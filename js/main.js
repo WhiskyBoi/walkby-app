20e6ee51a7f58feaac6677196c793882e974294a
WhiskyBoi <mert.ayhan@haw-hamburg.de>
2025-11-01 15:11:56 +0100

    logos verbessert für projektkarte

--- js/main.js
+++ js/main.js
@@ -54,7 +54,7 @@ document.addEventListener('DOMContentLoaded', () => {
             document.body.appendChild(clone);
         }
     });
-    
+
     const dom = {
         pages: document.querySelectorAll('.page'),
         navButtons: document.querySelectorAll('.floating-nav-item'),
@@ -545,19 +545,32 @@ if (clip.author) {
                     <span>${project.address ? project.address.split(',')[0] : 'Kein Ort'}</span>
                 </div>
                 <div class="flex items-center gap-1">
-                    <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path d="M7 3.5A1.5 1.5 0 018.5 2h3A1.5 1.5 0 0113 3.5v1.586a1.5 1.5 0 01-.44 1.06L9.5 9.586V16.5a1.5 1.5 0 01-3 0V9.586L3.44 6.146A1.5 1.5 0 013 4.586V3.5z" /></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+                        <path d="M15.5 7.5a.5.5 0 01.444.89l-1.444.89a.5.5 0 01-.444-.89l1.444-.89zM16.444 8.39a.5.5 0 010 .89l-1.444.89a.5.5 0 01-.444-.89l1.444-.89zM16 12a1 1 0 01-1 1H2a1 1 0 01-1-1V6a1 1 0 011-1h1.172a3 3 0 002.12-.879l.88-.879A1 1 0 017 3h6a1 1 0 01.707.293l.879.879A3 3 0 0016.707 5H18a1 1 0 011 1v6a1 1 0 01-1 1h-2zM4 8a2 2 0 100 4 2 2 0 000-4z" />
+                    </svg>
                     <span>${project.media.length}/5 Medien</span>
                 </div>
             </div>
         </div>
         
-        <div class="flex-shrink-0 flex items-center justify-end gap-1 p-2 bg-gray-50 border-t border-gray-100">
+<div class="flex-shrink-0 flex items-center justify-end gap-1 p-2 bg-gray-50 border-t border-gray-100">
+            
+            <button data-action="play" title="Abspielen" class="p-2 rounded-full text-gray-600 hover:bg-gray-200 transition-colors">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clip-rule="evenodd" /></svg>
+            </button>
+
             <button data-action="edit-info" title="Infos bearbeiten" class="p-2 rounded-full text-gray-600 hover:bg-gray-200 transition-colors">
-                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"><path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828zM5 12V7a2 2 0 012-2h4l-2 2H7v5H5z"></path></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path d="M13.586 3.586a2 2 0 112.828 2.828l-1.172 1.172-2.828-2.828 1.172-1.172zM11.172 6.172L2.5 14.844V17.5h2.656l8.672-8.672-2.656-2.656z" />
+                </svg>
             </button>
+            
             <button data-action="edit-clips" title="Clips bearbeiten" class="p-2 rounded-full text-gray-600 hover:bg-gray-200 transition-colors">
-                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 24 24" fill="currentColor"><path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"></path></svg>
+                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z" />
+                </svg>
             </button>
+
             <button data-action="delete" title="Löschen" class="p-2 rounded-full text-red-600 hover:bg-red-100 transition-colors">
                 <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm4 0a1 1 0 012 0v6a1 1 0 11-2 0V8z" clip-rule="evenodd"></path></svg>
             </button>
@@ -569,6 +582,8 @@ if (clip.author) {
         if (mediaContainer) {
             mediaContainer.addEventListener('click', () => openPlayback(project.media));
         }
+
+        card.querySelector('[data-action="play"]').addEventListener('click', () => openPlayback(project.media));
         card.querySelector('[data-action="edit-info"]').addEventListener('click', () => openProjectModal(project));
         card.querySelector('[data-action="edit-clips"]').addEventListener('click', () => handleEditClips(project));
         card.querySelector('[data-action="delete"]').addEventListener('click', async () => {
